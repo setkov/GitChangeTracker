3de5a698d9f739d963d4bb56918c05f6e09c8019
Sergey Setkov <s.setkov@gmail.com>
2021-08-11 14:13:03 +0300

    remove debug output

--- main.go
+++ main.go
@@ -12,16 +12,13 @@ func main() {
 	parameters, err := Common.NewParameters()
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		fmt.Println("parameters", parameters)
 	}
 
 	trackService := TrackingService.NewTrackingService(parameters)
 	err = trackService.Track()
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		fmt.Println("track commit success")
 	}
 
+	fmt.Println("track commit success")
 }
